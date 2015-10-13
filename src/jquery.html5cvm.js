16e54c8c696804fe85d5e37eed2f82600f26580a
CoalaWeb <support@coalaweb.com>
2015-10-13 11:33:27 -0400

    Small bug fix affecting select and checkbox fields plus updated the demo.

--- src/jquery.html5cvm.js
+++ src/jquery.html5cvm.js
@@ -5,7 +5,7 @@
  * @author url          https://github.com/CoalaWeb
  * @author email        support@coalaweb.com
  * @version             1.0.0
- * @date                10-10-2015
+ * @date                13-10-2015
  * @copyright           Copyright (c) 2015 Steven Palmer All rights reserved.
  *
  * Dual licensed under the MIT and GPL licenses:
@@ -28,36 +28,35 @@
             stepMismatch: null,
             generic: null
         }, options);
-       
+
         return this.each(function () {
 
             //lets get our form ID
             var form = $(this).attr('id')
 
             //now to add some custom messages to our form
-            $('#'+form+' :input').on({
-                
+            $('#' + form + ' :input').on({
                 //lets start out validity check
                 invalid: function (e) {
 
                     //clear the custom validity value
                     e.target.setCustomValidity("");
-                    
+
                     //is the form element invalid?
                     if (!e.target.validity.valid) {
-                        
+
                         //initiate our msg variable
                         var msg;
-                        
+
                         //returns true if the element has no value but is a required field.
                         if (e.target.validity.valueMissing) {
-                            if ($(this).data("error-value-missing")){
+                            if ($(this).data("error-value-missing")) {
                                 msg = $(this).data("error-value-missing");
                             } else {
                                 msg = settings.valueMissing;
                             }
                         }
-                        
+
                         //returns true if the element’s value doesn't match its type attribute.
                         if (e.target.validity.typeMismatch) {
                             if ($(this).data("error-type-mismatch")) {
@@ -75,7 +74,7 @@
                                 msg = settings.patternMismatch;
                             }
                         }
-                        
+
                         //returns true if the element’s value exceeds its maxlength attribute.
                         if (e.target.validity.tooLong) {
                             if ($(this).data("error-pattern-too-long")) {
@@ -84,7 +83,7 @@
                                 msg = settings.tooLong;
                             }
                         }
-                        
+
                         //returns true if the element’s value is lower than its min attribute.
                         if (e.target.validity.rangeUnderflow) {
                             if ($(this).data("error-pattern-range-underflow")) {
@@ -93,7 +92,7 @@
                                 msg = settings.rangeUnderflow;
                             }
                         }
-                        
+
                         //returns true if the element’s value is higher than its max attribute.
                         if (e.target.validity.rangeOverflow) {
                             if ($(this).data("error-pattern-range-overflow")) {
@@ -102,7 +101,7 @@
                                 msg = settings.rangeUOverflow;
                             }
                         }
-                        
+
                         //returns true if the element’s value is invalid per its step attribute.
                         if (e.target.validity.stepMismatch) {
                             if ($(this).data("error-step-mismatch")) {
@@ -111,7 +110,7 @@
                                 msg = settings.stepMismatch;
                             }
                         }
-                        
+
                         //generic fall back message.
                         if (!msg) {
                             if ($(this).data("error-generic")) {
@@ -120,15 +119,19 @@
                                 msg = settings.generic;
                             }
                         }
-                        
+
                         //set the custom validty value to our custom message if we have one.
-                        if (msg){
+                        if (msg) {
                             e.target.setCustomValidity(msg);
                         }
-                        
+
                     }
                 },
                 input: function (e) {
+                    //clear the custom validty value
+                    e.target.setCustomValidity("");
+                },
+                change: function (e) {
                     //clear the custom validty value
                     e.target.setCustomValidity("");
                 }
